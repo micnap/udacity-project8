7874b7aa41cecf11809b1e77839c2032f425b1dc
Michelle Williamson <mwill31@uis.edu>
2018-08-19 21:18:41 -0400

    Initial commit - FB/Firebase Authentication is working

---
+++ app/src/main/java/com/mickeywilliamson/project8/GTApplication.java
@@ -0,0 +1,15 @@
+package com.mickeywilliamson.project8;
+
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class GTApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
