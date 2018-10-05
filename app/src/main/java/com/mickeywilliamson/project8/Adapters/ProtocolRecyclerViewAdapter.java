666af40c39440b3a89cbda6b4ad9067df6262126
Michelle Williamson <mickey@micnap.com>
2018-10-05 17:25:12 -0400

    Removes context import from recyclerview adapter

--- app/src/main/java/com/mickeywilliamson/project8/Adapters/ProtocolRecyclerViewAdapter.java
+++ app/src/main/java/com/mickeywilliamson/project8/Adapters/ProtocolRecyclerViewAdapter.java
@@ -1,6 +1,5 @@
 package com.mickeywilliamson.project8.Adapters;
 
-import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.widget.RecyclerView;
