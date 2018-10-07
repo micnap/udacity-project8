2751322a35f3bc04cb556165e1dc2101685d04d8
Michelle Williamson <mickey@micnap.com>
2018-10-06 22:30:47 -0400

    Adds firebase support for preferences

--- app/src/main/java/com/mickeywilliamson/project8/Activities/UserPreferenceActivity.java
+++ app/src/main/java/com/mickeywilliamson/project8/Activities/UserPreferenceActivity.java
@@ -1,15 +1,24 @@
 package com.mickeywilliamson.project8.Activities;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.mickeywilliamson.project8.R;
 
-public class UserPreferenceActivity extends AppCompatActivity {
+import sharefirebasepreferences.crysxd.de.lib.SharedFirebasePreferences;
+import sharefirebasepreferences.crysxd.de.lib.SharedFirebasePreferencesContextWrapper;
+
+
+public class UserPreferenceActivity extends AppCompatActivity implements FirebaseAuth.AuthStateListener, SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private static final String TAG = "prefactivity";
+    private SharedFirebasePreferences mPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,69 @@ public class UserPreferenceActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FirebaseAuth.getInstance().addAuthStateListener(this);
+
+        getPrefs(FirebaseAuth.getInstance());
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mPreferences != null) {
+            mPreferences.keepSynced(true);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mPreferences != null) {
+            mPreferences.keepSynced(false);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mPreferences != null) {
+            mPreferences.unregisterOnSharedPreferenceChangeListener(this);
+        }
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(new SharedFirebasePreferencesContextWrapper(newBase));
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+
+    }
+
+    @Override
+    public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+        getPrefs(firebaseAuth);
+    }
+
+    private void getPrefs(FirebaseAuth firebaseAuth) {
+        if (firebaseAuth.getCurrentUser() != null) {
+            mPreferences = SharedFirebasePreferences.getDefaultInstance(this);
+            mPreferences.keepSynced(true);
+            mPreferences.registerOnSharedPreferenceChangeListener(this);
+            mPreferences.pull().addOnPullCompleteListener(new SharedFirebasePreferences.OnPullCompleteListener() {
+                @Override
+                public void onPullSucceeded(SharedFirebasePreferences preferences) {
+                    //showView();
+                }
+
+                @Override
+                public void onPullFailed(Exception e) {
+                    //showView();
+                    Toast.makeText(UserPreferenceActivity.this, "Fetch failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
 }
